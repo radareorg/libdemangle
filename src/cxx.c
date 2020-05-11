859b45ded2c7fa17e7b12c64f37bf6a07b3186c1
pancake <pancake@nopcode.org>
2020-05-11 18:23:10 +0200

    Handle the @@GLIBC suffix in cxx symbols like it was done in r2land

--- src/cxx.c
+++ src/cxx.c
@@ -44,8 +44,15 @@ R_API char *ldmg_cxx(const char *str) {
 	}
 	// remove CXXABI suffix
 	char *cxxabi = strstr (p, "@@CXXABI");
+	char *glibcxx = strstr (p, "@GLIBCXX");
 	if (cxxabi) {
 		*cxxabi = '\0';
+	} else if (glibcxx) {
+		if (p < glibcxx && glibcxx[-1] == '@') {
+			glibcxx[-1] = '\0';
+		} else {
+			*glibcxx = '\0';
+		}
 	}
 #if WITH_GPL
 	char *out = cplus_demangle_v3 (p, flags);
