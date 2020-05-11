37b29527a854c6524604a0dca53e3fd21428b7de
pancake <pancake@nopcode.org>
2020-05-11 15:28:27 +0200

    Add more sample strings

--- bindings/v/ldmg/lib.v
+++ bindings/v/ldmg/lib.v
@@ -6,11 +6,22 @@ module ldmg
 
 #include <ldmg.h>
 
+fn C.ldmg_byname(name byteptr) C.LdmgCall
 fn C.ldmg_cxx(a byteptr) byteptr
 fn C.ldmg_swift(a byteptr) byteptr
 fn C.ldmg_swift_sys(a byteptr) byteptr
 
+pub type LdmgCall C.LdmgCall
+
 // cpub var C.ldmg_version byteptr 
+
+pub fn by_name(s string) ?C.LdmgCall {
+	r := C.ldmg_byname(s.str)
+	if isnil(r) {
+		return error('Oops')
+	}
+	return r
+}
 
 pub fn swift(s string) string {
 	t := C.ldmg_swift(s.str)
--- bindings/v/main.v
+++ bindings/v/main.v
@@ -10,6 +10,13 @@ fn d(s string) {
 }
 
 fn main() {
+	dd := ldmg.by_name('swift') or {
+		eprintln(err)
+		exit(1)
+	}
+
+	println(dd('_TWPu0_Rq_Ss14CollectionType_GVSs17MapCollectionViewq_q0__Ss23_CollectionDefaultsTypeSs_8'))
+
 	d('_TWPu0_Rq_Ss14CollectionType_GVSs17MapCollectionViewq_q0__Ss23_CollectionDefaultsTypeSs_8')
 	println(ldmg.version())
 }
